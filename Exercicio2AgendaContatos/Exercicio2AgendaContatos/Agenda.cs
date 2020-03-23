911703d8a0621698e5e4ba51ab4c6f1444b1cfdc
Gustavo Haubrich <gustavohaubrich15@gmail.com>
2020-03-23 16:29:32 -0300

    Add files via upload

---
+++ Exercicio2AgendaContatos/Exercicio2AgendaContatos/Agenda.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio2AgendaContatos
+{
+    class Agenda
+    {
+        public List<string> ListaCompleta { get; private set; }
+
+        public Agenda()
+        {
+            ListaCompleta = new List<string>();
+        }
+        public void AdicionarContato(string contato)
+        {
+            ListaCompleta.Add(contato);
+        }
+
+        public void RemoverContato(string contato)
+        {
+            ListaCompleta.Remove(contato);
+        }
+
+        public List<String> BuscarContatoPorLetra(char letra)
+        {
+            List<String> busca = new List<string>();
+
+            foreach (string contato in ListaCompleta)
+            {
+                if (char.ToLower(contato[0]) == char.ToLower(letra))
+                {
+                    busca.Add(contato);
+                }
+            }
+
+            return busca;
+        }
+    }
+}
---
+++ Exercicio2AgendaContatos/Exercicio2AgendaContatos/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Exercicio2AgendaContatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Agenda agenda = new Agenda();
+            char digitado = 's';
+            while (digitado != 'e')
+            {
+                Console.WriteLine("a. Ver lista completa");
+                Console.WriteLine("b. Buscar contatos por letra");
+                Console.WriteLine("c. Adicionar contato");
+                Console.WriteLine("d. Remover contato");
+                Console.WriteLine("e. Sair");
+                Console.Write("\nDigite uma opcao:");
+                digitado = char.Parse(Console.ReadLine());
+                switch (digitado)
+                {
+                    case 'a':
+                        Console.WriteLine("\nLista de contatos");
+                        foreach (string contato in agenda.ListaCompleta)
+                        {
+                            Console.WriteLine(contato);
+                        }
+
+                        Console.WriteLine("\n");
+                        break;
+                    case 'b':
+                        Console.Write("\nDigite uma letra:");
+                        string letra = Console.ReadLine();
+                        Console.WriteLine("\n Resultados da busca:");
+                        foreach (string busca in agenda.BuscarContatoPorLetra(letra[0]))
+                        {
+                            Console.WriteLine(busca);
+                        }
+                        Console.WriteLine("\n");
+                        break;
+                    case 'c':
+                        Console.Write("\nDigite um nome:");
+                        string nome = Console.ReadLine();
+                        agenda.AdicionarContato(nome);
+                        Console.WriteLine("\n");
+                        break;
+                    case 'd':
+                        Console.Write("\nDigite um nome:");
+                        string removerNome = Console.ReadLine();
+                        agenda.RemoverContato(removerNome);
+                        Console.WriteLine("\n");
+                        break;
+                    case 'e':
+                        Console.WriteLine("Saindo.....\n");
+                        break;
+                    default:
+                        Console.WriteLine("Opcao invalida!!!!\n");
+                        break;
+                }
+            }
+        }
+    }
+}
